840b3f0ad7934bcb4cb68bb66369c066fe9a8c16
HenryJack <HEJACK@s.sfusd.edu>
2018-10-23 14:19:00 -0700

    Final Version of Old MacDonald

--- Chick.java
+++ Chick.java
@@ -1,4 +1,19 @@
-class Chick //implements Animal 
+class Chick implements Animal//implements Animal 
 {     
-     //your code here
+	private String myType;
+ 	private String mySound;
+ 	public Chick(String type, String sound, String sound2) {
+ 		myType = type;
+		if(Math.random() > .5) {
+			mySound = sound2;
+		} else {
+			mySound = sound;
+		}
+ 	}
+ 	public String getSound(){
+ 		return mySound;
+ 	}
+ 	public String getType(){
+ 		return myType;
+ 	}
 }
